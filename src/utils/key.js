81f629cd8710055dc63cbe07adbd3945230c59bb
github username <me@joshuahill.dev>
2020-01-13 14:53:23 -0600

    heroku only for keys

---
+++ src/utils/key.js
@@ -0,0 +1,5 @@
+const API_KEY = "4491611db16e860917baaff551932c09";
+const mapBox = "pk.eyJ1IjoiamxoaWxsODQiLCJhIjoiY2syd2E0OTZhMGNuZTNjbnh1c3JtMmRlbCJ9.bZ5yo16yzTIjaOM6IxH23g"
+
+module.exports.weather = API_KEY;
+module.exports.maps = mapBox;
